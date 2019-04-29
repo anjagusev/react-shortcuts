2bb3c80a9cc2820c4d8b7b86ace101b5ea91e140
anjagusev <anja.gusev@gmail.com>
2019-04-29 08:49:36 -0400

    adding react router

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Shortcuts from "./Shortcuts";
 import App from "./App";
 import NotFound from "./NotFound";
+import Analytics from "react-router-ga";
 
 const Router = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Shortcuts} />
-      <Route path="/:Id" component={Shortcuts} />
-      <Route component={NotFound} />
-    </Switch>
+    <Analytics id="UA-41136002-4" debug>
+      <Switch>
+        <Route exact path="/" component={Shortcuts} />
+        <Route path="/:Id" component={Shortcuts} />
+        <Route component={NotFound} />
+      </Switch>
+    </Analytics>
   </BrowserRouter>
 );
 
